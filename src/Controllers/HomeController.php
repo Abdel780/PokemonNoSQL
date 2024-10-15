9ffff136b37adf7dafb2dc4f06493824acfd549d
Abdel780 <abdelkarim27200@gmail.com>
2024-10-15 15:03:36 +0200

    fin du ticket afficher les pokemon

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -3,11 +3,16 @@
 namespace App\Controllers;
 
 use App\Utils\AbstractController;
+use App\Models\Pokemon;
 
 class HomeController extends AbstractController
 {
     public function index()
     {
+
+        $task = new Pokemon(null, null, null, null, null);
+                $arrayPokemons = $task->getAll();
+
         require_once(__DIR__ . '/../Views/home.view.php');
     }
 }
